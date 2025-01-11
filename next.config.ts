5203c9a90aee75b1ad21092d959b881c2309b789
Dongjie Wu <dongjie.wu@protonmail.com>
2025-01-11 23:05:14 +1000

    first commit

---
+++ next.config.ts
@@ -0,0 +1,28 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          },
+        ],
+      },
+    ]
+  },
+};
+
+export default nextConfig;
